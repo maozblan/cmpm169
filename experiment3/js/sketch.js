5aa82aff7a3b25f3b55a37de3bcc90598ef0d6c8
Lyssa Li <98376314+maozblan@users.noreply.github.com>
2024-01-26 14:06:59 -0800

    updated homepage & started experiment 3

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -2,30 +2,14 @@
 // Author: Your Name
 // Date:
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
+const axiom = "F";
+const rule = {
+  "F": "F+G",
+  "G": "F-G",
 }
+let sentence = axiom;
+let len = 10;
 
-// setup() function is called once when the program starts
 function setup() {
     // place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
@@ -36,32 +20,36 @@ function setup() {
         console.log("Resizing...");
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+    background(0);
+    stroke(255);
+    for (let i = 0; i < 9; i++) {
+        generate();
+    }
+    drawLSystem();
 }
 
-// draw() function is called repeatedly, it's the main animation loop
-function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+// L-systems base from https://editor.p5js.org/wmodes/sketches/UoSg_99pH
+function generate() {
+    let nextSentence = "";
+    for (let i = 0; i < sentence.length; i++) {
+        let current = sentence.charAt(i);
+        nextSentence += rule[current] || current;
+    }
+    sentence = nextSentence;
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function drawLSystem() {
+  translate(width/2, height/2);
+  for (let i = 0; i < sentence.length; i++) {
+    let current = sentence.charAt(i);
+    if (current === "F" || current === "G") {
+      line(0, 0, -len, 0);
+      translate(-len, 0);
+    } else if (current === "+") {
+      rotate(radians(80));
+    } else if (current === "-") {
+      rotate(radians(-80));
+    }
+  }
 }
