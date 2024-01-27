00c80dd760cb8fb026c47aa4ac266aa3a2136d7c
Lyssa Li <98376314+maozblan@users.noreply.github.com>
2024-01-26 16:37:48 -0800

    got it into a class

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -1,14 +1,10 @@
 // sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Lyssa Li
+// Date: Jan 29th
 
-const axiom = "F";
-const rule = {
-  "F": "F+G",
-  "G": "F-G",
-}
-let sentence = axiom;
-let len = 10;
+// L-systems base from https://editor.p5js.org/wmodes/sketches/UoSg_99pH
+
+let boop;
 
 function setup() {
     // place our canvas, making it fit our container
@@ -23,33 +19,54 @@ function setup() {
 
     background(0);
     stroke(255);
-    for (let i = 0; i < 9; i++) {
-        generate();
-    }
-    drawLSystem();
+
+    boop = new DragonCurve(5);
 }
 
-// L-systems base from https://editor.p5js.org/wmodes/sketches/UoSg_99pH
-function generate() {
-    let nextSentence = "";
-    for (let i = 0; i < sentence.length; i++) {
-        let current = sentence.charAt(i);
-        nextSentence += rule[current] || current;
-    }
-    sentence = nextSentence;
+function mousePressed() {
+    console.log('boop');
+    boop.drawLine();
 }
 
-function drawLSystem() {
-  translate(width/2, height/2);
-  for (let i = 0; i < sentence.length; i++) {
-    let current = sentence.charAt(i);
-    if (current === "F" || current === "G") {
-      line(0, 0, -len, 0);
-      translate(-len, 0);
-    } else if (current === "+") {
-      rotate(radians(80));
-    } else if (current === "-") {
-      rotate(radians(-80));
+class DragonCurve {
+    constructor(num) {
+        this.rules = {
+            "F": "F+G",
+            "G": "F-G",
+        }
+        this.sentence = "F";
+        this.len = 10;
+        this.angle = 85;
+        this.iterations = num;
+        for (let i = 0; i < this.iterations; ++i) {
+            this.generate();
+        }
+
+        // for iterating the lines
+        this.index = 0;
     }
-  }
+
+    generate() {
+        let nextSentence = "";
+        for (let i = 0; i < this.sentence.length; i++) {
+            let current = this.sentence.charAt(i);
+            nextSentence += this.rules[current] || current;
+        }
+        this.sentence = nextSentence;
+    }
+
+    drawLine() {
+        translate(width/2, height/2);
+        while (this.index < this.sentence.length) {
+            let current = this.sentence.charAt(this.index++);
+            if (current === "F" || current === "G") {
+                line(0, 0, -this.len, 0);
+                translate(-this.len, 0);
+            } else if (current === "+") {
+                rotate(radians(this.angle));
+            } else if (current === "-") {
+                rotate(radians(-this.angle));
+            }
+        }
+    }
 }
