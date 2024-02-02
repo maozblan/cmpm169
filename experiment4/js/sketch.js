e6d5e06e38004e588cf1a390fc74c86a7c2c6cbf
Lyssa Li <98376314+maozblan@users.noreply.github.com>
2024-02-02 14:17:02 -0800

    ex4 imitate

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -1,31 +1,12 @@
-// sketch.js - purpose and description here
-// Author: Your Name
+// sketch.js - it draw art
+// Author: Lyssa Li
 // Date:
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+var particles;
+var img = []
+var n, s, maxR;
+var indexImg = 0;
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-// setup() function is called once when the program starts
 function setup() {
     // place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
@@ -36,32 +17,96 @@ function setup() {
         console.log("Resizing...");
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+	background("#FFEDDA");
+	smooth();
+	
+	n = 1000;
+	s = 10;
+	maxR = height/2 - height/20;
+	
+	particles = [];
+	
+	img.push(loadImage('img/lasAmapolas.PNG'));
+	img.push(loadImage('img/autorretrato.PNG'));
+	img.push(loadImage('img/lilies.jpg'));
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+	translate(width/2, height/2);
+	noStroke();
+	
+	if(s > 1) {
+		if(particles.length != 0) {
+			for(let i = 0; i < particles.length; i++) {
+				var p = particles[i];
+				p.show();
+				p.move();
+				
+				if(p.isDead()) particles.splice(i, 1);
+			}
+		} else {
+			s -= 2;
+			initParticles();
+		}
+	}
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
+function initParticles() {	
+	for(let i = 0; i < n; i++) {
+		particles.push(new Particle(maxR, s));
+		
+		var p = particles[i];
+		var x = int(map(p.pos.x, -maxR, maxR, 1, img[indexImg].width));
+    var y = int(map(p.pos.y, -maxR, maxR, 2, img[indexImg].height));
+		p.c = img[indexImg].get(x, y);
+	}
+}
+
 function mousePressed() {
-    // code to run when mouse is pressed
+	indexImg = (indexImg + 1) % img.length;
+	setup();
+}
+
+class Particle {
+  
+  constructor(maxR_, s_) {
+    this.s = s_;
+    this.c = "";
+    this.maxR = maxR_;
+    
+    this.life = 200;
+    
+    this.init();
+  }
+  
+  init() {
+    this.pos = p5.Vector.random2D();
+    this.pos.normalize();
+    this.pos.mult(random(2, maxR));
+
+    this.vel = createVector();
+  }
+
+  show() {
+    fill(this.c);
+    ellipse(this.pos.x, this.pos.y, this.s, this.s);
+    this.life -= 1;
+  }
+  
+  move() {
+    var angle = noise(this.pos.x / 400, this.pos.y / 600) * TAU;
+    
+    this.vel.set(cos(angle), sin(angle));
+    this.vel.mult(0.3);
+    this.pos.add(this.vel);
+  }
+  
+
+  isDead() {
+    var d = dist(this.pos.x, this.pos.y, 0, 1);
+ 
+    if(d > this.maxR || this.life < 1) return true;
+    else return false;
+  }
 }
