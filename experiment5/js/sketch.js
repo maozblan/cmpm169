4b76f55f9d1c943cc1d8252a68c1ebc14a1a1fd0
Lyssa Li <98376314+maozblan@users.noreply.github.com>
2024-02-09 23:00:32 -0800

    ex5 steps 1 and 2

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -1,67 +1,115 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// sketch.js - the main sketch
+// Author: Lyssa Li
+// edited from https://openprocessing.org/sketch/890654
+// and with the help of chatGPT 
+// Date: 2024 02 12
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+var particles = [];
+const maxParticles = 400;
+const particlesPerRound = 50;
+let index = 0; // current index of next available particle
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
+	angleMode(DEGREES);
     canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height(), WEBGL);
     canvas.parent("canvas-container");
     // resize canvas is the page is resized
     $(window).resize(function() {
         console.log("Resizing...");
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+	for(var i = 0; i<maxParticles; i++){
+		particles.push(new particle);
+	}
+	noStroke();
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+	rotateX(-mouseY);
+	rotateY(mouseX);
+	offset = 0;
+	background(0);
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+    // set up lights
+    ambientLight(10);
+
+	for(var i = 0; i<particles.length; i++){
+		particles[i].update();
+	}
+ /*
+	for(var i = 0; i<particles.length; i++){
+		if(particles[i+offset].ded){
+			particles.splice(i+offset, 1);
+			offset--;
+			i++;
+		}
+	}
+    */
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
+class particle{
+	constructor(){
+		this.x = 0;
+		this.y = 0;
+		this.z = 0;
+		this.ded = true;    // DON'T PLAY ON CREATION
+		this.vel = [random(0, 0.5), random(0, 0.5), random(0, 0.5)];
+		this.total = 0.5/(sqrt((this.vel[0]*this.vel[0])+(this.vel[1]*this.vel[1])+(this.vel[2]*this.vel[2]))+random(-0.9, -0.3));
+		this.vel = [this.vel[0]*this.total*random([-1, 1]), this.vel[1]*this.total*random([-1, 1]), this.vel[2]*this.total*random([-1, 1])];
+		this.color = 255;
+		this.age = 0;
+        this.size = random(1, 3);
+        this.blink = false;
+        this.blinkCounter = 0;
+	}
+    
+    reset() {
+        this.x = 0;
+		this.y = 0;
+		this.z = 0;
+        this.ded = false;
+		this.vel = [random(0, 0.5), random(0, 0.5), random(0, 0.5)];
+		this.total = 0.5/(sqrt((this.vel[0]*this.vel[0])+(this.vel[1]*this.vel[1])+(this.vel[2]*this.vel[2]))+random(-0.9, -0.3));
+		this.vel = [this.vel[0]*this.total*random([-1, 1]), this.vel[1]*this.total*random([-1, 1]), this.vel[2]*this.total*random([-1, 1])];
+		this.color = 255;
+		this.age = 0;
+        this.size = random(1, 3);
+        this.blink = false;
+        this.blinkCounter = 0;
+    }
+
+	update() {
+        if (!this.ded) {
+            this.age += random(0.2, 2);
+            if(this.ded === false && 1000<this.age){
+                this.ded = true;
+            }
+            this.vel = [this.vel[0]*random(0.99, 1.001), this.vel[1]*random(0.99, 1.001), this.vel[2]*random(0.99, 1.001)];
+            this.x += this.vel[0];
+            this.y += this.vel[1];
+            this.z += this.vel[2];
+            translate(this.x, this.y, this.z);
+            emissiveMaterial(255, 255, 255, 255); // Set the emissive (glowing) color
+            if (random(0, 1) > 0.9995) {
+                this.blink = true;
+            }
+            if (this.blink) {
+                emissiveMaterial(0, 0, 0, 0);
+                if (this.blinkCounter++ > 15) {
+                    this.blink = false;
+                }
+            }
+            sphere(this.size, 8, 4);
+            translate(-this.x, -this.y, -this.z);
+        }
+	}
+}
+
 function mousePressed() {
-    // code to run when mouse is pressed
+    // refactor next group of particles
+    for(var i = 0; i<particlesPerRound; i++){
+        particles[index++ % maxParticles].reset();
+    }
 }
