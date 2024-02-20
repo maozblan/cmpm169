e150fd5b7f30e29699c4c8350db641c8eb109372
Lyssa Li <98376314+maozblan@users.noreply.github.com>
2024-02-19 17:12:58 -0800

    FINNNNN HAHAH

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -4,35 +4,43 @@
 
 // edited from https://openprocessing.org/sketch/1098610
 
-const size = 15;
+const size = 30;
+let img;
 
 let chars = [" ", "；", "。", "光", "空", "亮", "耀"], // [" ", "*", "•", "o", "0", "O", "●"],
 		data,
 		particles = [],
-		colors = [[0], [75], [150], [200], [250], [255, 200, 200], [225, 150, 150]];
+		colors;
 
 function setup() {
 	// place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
     let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
     canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
 
 	textFont("Century Schoolbook", size*1);
 	data = new Array(width/size*height/size).fill().map(() => 0);
-	fill(255);
+
+	// image data
+	colors = [];
+	for (let j=0; j<height; j+=size) {
+		for (let i=0; i<width; i+=size) {
+			colors.push(getAverageColor(i, j, size))
+		}
+	}
 }
+
+function preload() {
+    img = loadImage('img/starryNight.PNG');	// got too lazy to copy it over XD
+}
+
 
 function draw() {
 	background(0);
 	const ps = particles.sort((a, b) => a.vel.magSq()-b.vel.magSq());
 	let p;
 	for(let i = 0; i < data.length; i ++){
-		fill(...colors[data[i]]);
+		fill(...colors[i]);
 		text(chars[data[i]], i % (width/size) * size, Math.floor(i / (width/size)) * size);
 		let d = new p5.Vector(i % (width/size) * size, Math.floor(i / (width/size)) * size);
 		p = ps.filter(z => z.pos.dist(d) < 100); // Not ideal, but performance-wise it is a must
@@ -55,7 +63,6 @@ function draw() {
 
 		// lower
 		p.time -= Math.random(0.1, 2)
-		console.log(p.time);
 		if (p.time <= 0) {
 			console.log('e', particles)
 			particles.splice(particles.indexOf(p), 1);
@@ -72,3 +79,29 @@ function mouseDragged(){
 		});
 	}
 }
+
+// edited from chatGPT https://chat.openai.com/share/6ad70d5f-95cf-434b-a485-b77121985160
+function getAverageColor(x, y, areaSize) {
+	let totalR = 0;
+	let totalG = 0;
+	let totalB = 0;
+  
+	// Loop through the pixels in the specified area
+	for (let i = 0; i <= areaSize; i++) {
+	  for (let j = 0; j <= areaSize; j++) {
+		let pixelColor = img.get(x + i, y + j);
+  
+		// Accumulate the RGB values
+		totalR += red(pixelColor);
+		totalG += green(pixelColor);
+		totalB += blue(pixelColor);
+	  }
+	}
+  
+	// Calculate the average RGB values
+	let avgR = totalR / (areaSize * areaSize);
+	let avgG = totalG / (areaSize * areaSize);
+	let avgB = totalB / (areaSize * areaSize);
+  
+	return [avgR, avgG, avgB];
+  }
